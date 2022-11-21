1ab849b8615f38cf2530a62fd2738fb5937ddd30
rezamultiaha <takolkaki@gmail.com>
2022-11-21 13:34:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#5E8B7E',
+        secondary: '#969696',
+        three: '#080B10',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
